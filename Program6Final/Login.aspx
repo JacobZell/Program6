c681b665af7e84029b08bb19f94d04d36544e2bb
jacob zell <jzell897@gmail.com>
2020-11-05 15:10:33 -0500

    oi

---
+++ Program6Final/Login.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Program6Final.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Login ID="Login1" runat="server" OnAuthenticate="Login1_Authenticate">
+            </asp:Login>
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="text-align: center" Text="Create User" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Program6Final/Login.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+namespace Program6Final
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            if (Membership.ValidateUser(Login1.UserName.Trim(), Login1.Password.Trim()))
+            {
+                FormsAuthentication.RedirectFromLoginPage(Login1.UserName.Trim(), false);
+                Label1.Visible = true;
+                Label1.Text = "You are logged on.";
+            }
+            else
+            {
+                MembershipUser user = Membership.GetUser(Login1.UserName.Trim());
+                Label1.Visible = true;
+                Label1.Text = "Locked out value:" + user.IsLockedOut.ToString();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("addUser.aspx");
+        }
+    }
+}
